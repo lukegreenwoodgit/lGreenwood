4c548e0c31b313f341e83dece962e880e7fee7f9
Luke Greenwood <b5008370@my.shu.ac.uk>
2019-04-22 16:34:30 +0100

    Created login and register

--- reviewApp/reviews/urls.py
+++ reviewApp/reviews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
 	path('products/', views.products, name='reviews-products'),
 	path('about/', views.about, name='reviews-about'),
 	path('contact/', views.contact, name='reviews-contact'),
+	path('register/', user_views.register, name='register'),
 ]
