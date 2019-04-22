940a56898da847707bf3d10d69d163e236104b8c
Luke Greenwood <b5008370@my.shu.ac.uk>
2019-04-22 23:40:21 +0100

    Added database, login and register

--- reviewApp/reviewApp/settings.py
+++ reviewApp/reviewApp/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'reviewApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reviewapp',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -123,3 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'reviews-home'
