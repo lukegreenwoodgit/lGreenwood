06d8a47c9fdf3ae38593c3ba61d7798ff39c2cfd
Luke Greenwood <b5008370@my.shu.ac.uk>
2019-04-23 17:24:13 +0100

    updated some of the previously created models

--- reviewApp/reviews/models.py
+++ reviewApp/reviews/models.py
@@ -4,21 +4,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Product(models.Model):
-	name = models.CharField(default = '', max_length = 255)
-	brand = models.CharField(default = '', max_length = 255)
-	avg_cost = models.DecimalField(default = 00.00, max_digits = 4, decimal_places = 2)
-	category = models.ForeginKey(Category, on_delete = models.CASCADE)
-	date_released = models.DateField(default = timezone.now)
-	description = models.TextField()
-	photo = models.ImageField(default = 'default.jpg', upload_to = 'product_pics')
-
 class Category(models.Model):
 	name = models.CharField(default = '', max_length = 255)
 	description = models.TextField()
 
+class Product(models.Model):
+	name = models.CharField(default = '', max_length = 255)
+	brand = models.CharField(default = '', max_length = 255)
+	avg_cost = models.DecimalField(default = 00.00, max_digits = 4, decimal_places = 2)
+	category = models.ForeignKey(Category, on_delete = models.CASCADE)
+	date_released = models.DateField(default = timezone.now)
+	description = models.TextField()
+	photo = models.ImageField(default = 'default.jpg', upload_to = 'product_pics')
+
 class Review(models.Model):
-	author = models.ForeginKey(User, on_delete = models.CASCADE)
-	product = models.ForeginKey(Product, on_delete = models.CASCADE)
+	author = models.ForeignKey(User, on_delete = models.CASCADE)
+	product = models.ForeignKey(Product, on_delete = models.CASCADE)
+	rating = models.IntegerField(default = 0, max_length = 5)
 	review_text = models.TextField()
 	date = models.DateField(default = timezone.now)
