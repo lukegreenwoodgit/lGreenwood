75ae70feb84b55a348c168f525b3714578d2592a
Luke Greenwood <b5008370@my.shu.ac.uk>
2019-04-24 01:47:20 +0100

    Added review details page

--- reviewApp/reviews/views.py
+++ reviewApp/reviews/views.py
@@ -22,6 +22,9 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
 	model = Product
 
+class ReviewDetailView(DetailView):
+	model = Review
+
 	# def get_context_data(self, **kwargs):
 	# 	context = super().get_context_data(**kwargs)
 	# 	context['reviews'] = Review.objects.filter(product_id = self.kwargs['pk'])
